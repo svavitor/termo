15a9bd2beb6a263d12a788f596eb37a049e89df2
Vitor Senna <vitor.senna@imd.ufrn.br>
2023-12-09 12:55:48 -0300

    criand e adicionando modelo de ficha ao fichas_api

--- backend/fichas_api/models.py
+++ backend/fichas_api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Ficha(models.Model):
+    cpf_cnpj = models.BigIntegerField(default=False)
+    nome = models.CharField(max_length=255)
+    identidade = models.BigIntegerField(default=False)
+    profissao = models.CharField(max_length=200)
+    naturalidade = models.CharField(max_length=200)
+    estado_civil = models.CharField(max_length=255)
+    endereco = models.TextField(max_length=500) #Temporario
